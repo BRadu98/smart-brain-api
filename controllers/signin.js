f3c0ce05afa54379733952d413c55371657ffea0
Baloi Radu <bradu2020@gmail.com>
2021-05-27 01:58:22 +0100

    upd

--- controllers/signin.js
+++ controllers/signin.js
@@ -51,11 +51,19 @@ const signToken = (email) => {
   return jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '2 days'});
 }
 
+const setToken = (key, value) => { //(token, id)
+  return Promise.resolve(redisClient.set(key, value))
+}
+
 const createSessions = (user) => {
   //JWT token, return user data
   const { email, id } = user;
   const token = signToken(email);
-  return { success: 'true', userId: id, token }
+  return setToken(token, id)
+    .then(() => { 
+      return { success: 'true', userId: id, token }
+    })
+    .catch(console.log)
 }
 
 //Main handler - only this should return a response
